72b417410b5d057fe0d94331edbc3793295ad634
waki285 <suzuneu@suzuneu.com>
2023-11-17 14:05:17 +0900

    keiryouka

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use rand::{Rng, thread_rng};
 use rand::distributions::Alphanumeric;
 
 fn main() {
+  let mut rng = thread_rng();
   loop {
-    let mut rng = thread_rng();
     let chars: String = (0..16).map(|_| rng.sample(Alphanumeric) as char).collect();
     println!("https://pay.paypay.ne.jp/{}", chars);
   }
